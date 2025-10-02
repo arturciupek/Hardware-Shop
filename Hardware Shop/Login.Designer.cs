030816ace9204e5692de744672067a68e78dcb27
arturciupek <arturtciupek18@gmail.com>
2025-10-02 15:12:37 +0200

    Add login form design.

--- Hardware Shop/Login.Designer.cs
+++ Hardware Shop/Login.Designer.cs
@@ -30,27 +30,108 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Label();
+            this.UserTb = new System.Windows.Forms.TextBox();
+            this.PassTb = new System.Windows.Forms.TextBox();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 26.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(406, 104);
+            this.label1.Location = new System.Drawing.Point(311, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(389, 62);
+            this.label1.Size = new System.Drawing.Size(635, 98);
             this.label1.TabIndex = 0;
             this.label1.Text = "Hardware Shop";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(481, 200);
+            this.pictureBox1.Image = global::Hardware_Shop.Properties.Resources.NicePng_construction_tools_png_1452311;
+            this.pictureBox1.Location = new System.Drawing.Point(399, 201);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(216, 162);
+            this.pictureBox1.Size = new System.Drawing.Size(449, 382);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(54, 670);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 68);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(54, 804);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(275, 68);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            // 
+            // Close
+            // 
+            this.Close.AutoSize = true;
+            this.Close.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Close.ForeColor = System.Drawing.Color.White;
+            this.Close.Location = new System.Drawing.Point(1178, -1);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(72, 68);
+            this.Close.TabIndex = 4;
+            this.Close.Text = "X";
+            // 
+            // UserTb
+            // 
+            this.UserTb.Font = new System.Drawing.Font("Times New Roman", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.UserTb.Location = new System.Drawing.Point(399, 676);
+            this.UserTb.Name = "UserTb";
+            this.UserTb.Size = new System.Drawing.Size(449, 62);
+            this.UserTb.TabIndex = 5;
+            // 
+            // PassTb
+            // 
+            this.PassTb.Font = new System.Drawing.Font("Times New Roman", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PassTb.Location = new System.Drawing.Point(399, 810);
+            this.PassTb.Name = "PassTb";
+            this.PassTb.Size = new System.Drawing.Size(449, 62);
+            this.PassTb.TabIndex = 6;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.BackColor = System.Drawing.Color.White;
+            this.LoginBtn.Font = new System.Drawing.Font("Times New Roman", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LoginBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.LoginBtn.Location = new System.Drawing.Point(497, 953);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(229, 80);
+            this.LoginBtn.TabIndex = 7;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(543, 1064);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 53);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Clear";
             // 
             // Login
             // 
@@ -59,6 +140,13 @@
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.ClientSize = new System.Drawing.Size(1249, 1240);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.PassTb);
+            this.Controls.Add(this.UserTb);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -75,6 +163,13 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Close;
+        private System.Windows.Forms.TextBox UserTb;
+        private System.Windows.Forms.TextBox PassTb;
+        private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.Label label4;
     }
 }
 
