62e11cced4e67cefb53f3ffd49d3fcaa83b3007f
arturciupek <arturtciupek18@gmail.com>
2026-03-05 13:04:49 +0100

    Add database schema script.

---
+++ Hardware Shop/Database/schema.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [CustomerId] INT           IDENTITY (2, 2) NOT NULL,
+    [CName]      VARCHAR (100) NULL,
+    [Phone]      VARCHAR (15)  NULL,
+    [Email]      VARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([CustomerId] ASC)
+);
+
+CREATE TABLE [dbo].[Products] (
+    [ProductID]   INT             IDENTITY (1, 1) NOT NULL,
+    [ProductName] VARCHAR (100)   NULL,
+    [Category]    VARCHAR (50)    NULL,
+    [Quantity]    INT             NULL,
+    [Price]       DECIMAL (10, 2) NULL,
+    PRIMARY KEY CLUSTERED ([ProductID] ASC)
+);
+
+CREATE TABLE [dbo].[Sales] (
+    [SaleID]       INT             IDENTITY (3, 3) NOT NULL,
+    [CustomerID]   INT             NULL,
+    [CustomerName] VARCHAR (50)    NULL,
+    [ProductID]    INT             NULL,
+    [ProductName]  VARCHAR (50)    NULL,
+    [QuantitySold] INT             NULL,
+    [TotalAmount]  DECIMAL (10, 2) NULL,
+    [SaleDate]     DATETIME        NULL,
+    PRIMARY KEY CLUSTERED ([SaleID] ASC),
+    FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([CustomerId]),
+    FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Products] ([ProductID])
+);
--- Hardware Shop/Login.Designer.cs
+++ Hardware Shop/Login.Designer.cs
@@ -129,7 +129,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(399, 201);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(449, 382);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
